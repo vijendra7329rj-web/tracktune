d12107d9f49a64a4a765c9f4fdb44fbc5c4d70a5
anuj yadav <vijendra7329rj@gmail.com>
2026-05-23 14:33:41 +0530

    Update app.ts
    fix express route

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -39,7 +39,7 @@ app.use("/api", router);
 const frontendPath = path.join(__dirname, "../../../soundtrace/dist");
 app.use(express.static(frontendPath));
 
-app.get("*", (_req, res) => {
+app.get("/{*splat}", (_req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
